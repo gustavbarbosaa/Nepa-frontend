f0d03a60c81d2fd3635e16580fbed0185c1b318c
gustavbarbosaa <gustavoaraujohab@gmail.com>
2025-05-24 22:46:28 -0300

    feat: cria page de login

---
+++ src/app/features/auth/pages/login/login.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { TextHeaderComponent } from '../../components/text-header/text-header.component';
+
+@Component({
+  selector: 'app-login-page',
+  imports: [TextHeaderComponent],
+  templateUrl: './login.page.html',
+  styleUrl: './login.page.css',
+})
+export class LoginPage {}
