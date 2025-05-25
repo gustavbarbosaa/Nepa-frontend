0e7cc33a02c6fed07ff90a98670ba11fcc0a245a
gustavbarbosaa <gustavoaraujohab@gmail.com>
2025-05-24 22:45:15 -0300

    feat: componente de input para formulario de autenticacao criado

---
+++ src/app/features/auth/components/input-form/input-form.component.ts
@@ -0,0 +1,63 @@
+import { Component, input, OnInit, signal } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import {
+  heroAcademicCap,
+  heroEye,
+  heroEyeSlash,
+  heroLockClosed,
+  heroDevicePhoneMobile,
+  heroUser,
+  heroAtSymbol,
+} from '@ng-icons/heroicons/outline';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-input-form',
+  imports: [NgIcon, FormsModule],
+  templateUrl: './input-form.component.html',
+  styleUrl: './input-form.component.css',
+  viewProviders: [
+    provideIcons({
+      heroAcademicCap,
+      heroEye,
+      heroEyeSlash,
+      heroLockClosed,
+      heroDevicePhoneMobile,
+      heroUser,
+      heroAtSymbol,
+    }),
+  ],
+})
+export class InputFormComponent implements OnInit {
+  id = input.required<string>();
+  label = input.required<string>();
+  type = input.required<string>();
+  initialIcon = input.required<string>();
+  finalIcon = input<string>('heroEye');
+  isPassword = input<boolean>(false);
+
+  typeInput = signal<string>('');
+  finalIconInput = signal<string>('');
+
+  ngOnInit(): void {
+    this.typeInput.set(this.type());
+    this.finalIconInput.set(this.finalIcon()!);
+  }
+
+  togglePassword(): void {
+    console.log('entrei');
+    if (this.isPassword()) {
+      console.log('entrei 2');
+      const newTypeInput =
+        this.typeInput() === 'password' ? 'text' : 'password';
+
+      console.log(newTypeInput);
+
+      this.typeInput.set(newTypeInput);
+
+      this.finalIconInput.set(
+        newTypeInput === 'password' ? 'heroEye' : 'heroEyeSlash'
+      );
+    }
+  }
+}
