5790ef64c72cc0167f534230a027b22a59c6ba69
gustavbarbosaa <gustavoaraujohab@gmail.com>
2025-05-28 13:40:12 -0300

    feat: atualiza configurações do ESLint, adiciona novos plugins e ajusta importações no componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginPage } from '@features/auth/pages/login/login.page';
-import { InputFormComponent } from '@shared/components/input-form/input-form.component';
-import { LogoComponent } from '@shared/components/logo/logo.component';
 import { RegisterPage } from '@features/auth/pages/register/register.page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginPage, RegisterPage],
+  imports: [RouterOutlet, RegisterPage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
