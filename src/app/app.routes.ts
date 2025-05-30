b72f2ce534d676e07ebe17aac71a3b00efa35391
NetLine-DEV <niulanio@netlinetelecom.com.br>
2025-05-30 16:34:33 -0300

    feat: cria rotas de auth e home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./features/auth/pages/auth.routes').then(r => r.authRoutes),
+  },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./features/home/pages/home.routes').then(r => r.homeRoutes),
+  },
+];
