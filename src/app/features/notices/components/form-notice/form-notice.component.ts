cb21d24b3cdae41fb542c857faea05d53bbe9b42
unknown <gustavoaraujohab@gmail.com>
2025-07-04 18:07:20 -0300

    feat: insere os campos do formulario de submeter edital

--- src/app/features/notices/components/form-notice/form-notice.component.ts
+++ src/app/features/notices/components/form-notice/form-notice.component.ts
@@ -1,21 +1,60 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
 import {
+  Component,
+  inject,
+  OnInit,
+  signal,
+  WritableSignal,
+} from '@angular/core';
+import {
+  FormControl,
   FormGroup,
   NonNullableFormBuilder,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { ButtonComponent } from '@shared/components/button/button.component';
+import { InputFormComponent } from '@shared/components/input-form/input-form.component';
+import { SelectFormComponent } from '@shared/components/select-form/select-form.component';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { FileUpload, FileUploadEvent } from 'primeng/fileupload';
 
 @Component({
   selector: 'app-form-notice',
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    ToastModule,
+    InputFormComponent,
+    SelectFormComponent,
+    ButtonComponent,
+    ReactiveFormsModule,
+    FileUpload,
+  ],
   templateUrl: './form-notice.component.html',
   styleUrl: './form-notice.component.css',
+  providers: [MessageService],
 })
 export class FormNoticeComponent implements OnInit {
   form!: FormGroup;
   formBuilder = inject(NonNullableFormBuilder);
+  messageService = inject(MessageService);
+  uploadedFiles: any[] = [];
+
+  admins: WritableSignal<{ label: string; value: string }[]> = signal([]);
+
+  onUpload(event: FileUploadEvent): void {
+    for (const file of event.files) {
+      this.uploadedFiles.push(file);
+    }
+
+    this.messageService.add({
+      severity: 'info',
+      summary: 'File Uploaded',
+      detail: '',
+    });
+  }
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
@@ -24,14 +63,28 @@ export class FormNoticeComponent implements OnInit {
       admin_id: ['', [Validators.required]],
       arquivo: [null, Validators.required],
     });
+
+    this.admins.set([
+      { label: 'Selecione um administrador', value: '' },
+      {
+        label: 'Exemplo',
+        value: '781aa5fa-0152-4f80-a7b9-34d26f3ad480',
+      },
+    ]);
   }
 
   onFileChange(event: Event): void {
     const input = event.target as HTMLInputElement;
 
-    if (!input.files && input.files.length < 0) return;
+    if (!input.files || input.files.length === 0) return;
 
     this.form.patchValue({ arquivo: input.files[0] });
     this.form.get('arquivo')?.updateValueAndValidity();
   }
+
+  getControl<T = string>(controlName: string): FormControl<T> {
+    return this.form.get(controlName) as FormControl<T>;
+  }
+
+  onSubmit(): void {}
 }
--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -2,10 +2,11 @@ import { RouterOutlet } from '@angular/router';
 import { Component, computed, inject } from '@angular/core';
 import { TokenService } from '@core/services/token/token.service';
 import { DrawerComponent } from '@shared/components/drawer/drawer.component';
+import { FormNoticeComponent } from '@features/notices/components/form-notice/form-notice.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, DrawerComponent],
+  imports: [RouterOutlet, DrawerComponent, FormNoticeComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
