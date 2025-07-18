5d7178bcfa04eb997fea90d540a15a863369af04
unknown <gustavoaraujohab@gmail.com>
2025-07-18 12:22:16 -0300

    feat: monta tela de listagem de alunos

--- src/app/features/students/pages/students/students.page.ts
+++ src/app/features/students/pages/students/students.page.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { TableStudentsComponent } from '@features/students/components/table-students/table-students.component';
+import { PagesLayoutComponent } from '@shared/components/pages-layout/pages-layout.component';
+import { HeaderStudentsComponent } from '@features/students/components/header-students/header-students.component';
 
 @Component({
   selector: 'app-students',
-  imports: [TableStudentsComponent],
+  imports: [
+    TableStudentsComponent,
+    PagesLayoutComponent,
+    HeaderStudentsComponent,
+  ],
   templateUrl: './students.page.html',
   styleUrl: './students.page.css',
 })
