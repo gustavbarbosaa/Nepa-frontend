29fbcf81136ef308c63108f648c5402e4813fd61
unknown <gustavoaraujohab@gmail.com>
2025-08-04 17:32:01 -0300

    implementa metodos do service de inscricoes

--- src/app/features/inscricoes/services/inscricoes/inscricoes.service.ts
+++ src/app/features/inscricoes/services/inscricoes/inscricoes.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '@env/environment';
+import { eProjectStatus } from '@features/projects/enums/status.enum';
 import { iInscricao } from '@shared/models/inscricao.model';
 import { Observable, take } from 'rxjs';
 
@@ -14,6 +15,36 @@ export class InscricoesService {
   getAllByProject(id: string): Observable<iInscricao[]> {
     return this.http
       .get<iInscricao[]>(`${this.apiUrl}/projetos/${id}/inscricoes/`)
+      .pipe(take(1));
+  }
+
+  getSubscriptionByIdProject(
+    idProject: string,
+    idSubscription: string
+  ): Observable<iInscricao> {
+    return this.http
+      .get<iInscricao>(
+        `${this.apiUrl}/projetos/${idProject}/inscricoes/${idSubscription}/`
+      )
+      .pipe(take(1));
+  }
+
+  subscribeInTheProject(id: string): Observable<iInscricao> {
+    return this.http
+      .post<iInscricao>(`${this.apiUrl}/projetos/${id}/inscricoes/`, id)
+      .pipe(take(1));
+  }
+
+  verifyStatusStudentInTheProject(
+    idProject: string,
+    idSubscription: string,
+    status: { status: eProjectStatus }
+  ): Observable<iInscricao> {
+    return this.http
+      .patch<iInscricao>(
+        `${this.apiUrl}/projetos/${idProject}/inscricoes/${idSubscription}/`,
+        status
+      )
       .pipe(take(1));
   }
 }
