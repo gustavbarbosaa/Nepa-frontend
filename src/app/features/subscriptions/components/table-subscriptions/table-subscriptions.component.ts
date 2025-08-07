f858de11fe6c315d1296e8313622870dae885f46
unknown <gustavoaraujohab@gmail.com>
2025-08-07 10:03:08 -0300

    implementa modais para aprovar ou reprovar alunos

--- src/app/features/subscriptions/components/table-subscriptions/table-subscriptions.component.ts
+++ src/app/features/subscriptions/components/table-subscriptions/table-subscriptions.component.ts
@@ -1,47 +1,66 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ToastService } from '@core/services/toast/toast.service';
-import { ProjectService } from '@features/projects/services/project/project.service';
-import { StudentSignalService } from '@features/students/services/student-signal/student-signal.service';
-import { StudentService } from '@features/students/services/student/student.service';
 import { SubscriptionsService } from '@features/subscriptions/services/subscriptions/subscriptions.service';
+import { ButtonComponent } from '@shared/components/button/button.component';
 import { TableListItemsComponent } from '@shared/components/table-list-items/table-list-items.component';
-import { iStudent } from '@shared/models/student.model';
+import { iInscricao } from '@shared/models/inscricao.model';
 import { MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { Dialog } from 'primeng/dialog';
+import { TagModule } from 'primeng/tag';
+import { Toast } from 'primeng/toast';
 
 @Component({
   selector: 'app-table-subscriptions',
-  imports: [TableListItemsComponent],
+  imports: [
+    TableListItemsComponent,
+    ButtonModule,
+    TagModule,
+    Dialog,
+    Toast,
+    ButtonComponent,
+  ],
   templateUrl: './table-subscriptions.component.html',
   styleUrl: './table-subscriptions.component.css',
   providers: [ToastService, MessageService],
 })
 export class TableSubscriptionsComponent implements OnInit {
-  private projectService = inject(ProjectService);
-  private studentService = inject(StudentService);
   private subscriptionsService = inject(SubscriptionsService);
-  private studentSignalService = inject(StudentSignalService);
   private toastService = inject(ToastService);
   private route = inject(ActivatedRoute);
 
-  allStudents = signal<iStudent[]>([]);
-  selectedStudent = signal<iStudent | null>(null);
   loading = signal<boolean>(false);
-  combinedData: any[] = [];
-  // combinedData: { user: iStudent; project: any; subscription: any }[] = [];
+  subscriptionsInProject = signal<iInscricao[]>([]);
+  subscriptionSelected = signal<iInscricao | null>(null);
+  visibleApprove = signal<boolean>(false);
+  visibleRepprove = signal<boolean>(false);
 
   ngOnInit(): void {
     const projectId = this.route.snapshot.paramMap.get('id');
     if (projectId) {
       this.fetchSubscriptionsWithDetails(projectId);
     } else {
-      console.error('ID do projeto não encontrado na URL.');
+      console.error('Erro ao buscar alunos inscritos no projeto!');
     }
   }
 
   fetchSubscriptionsWithDetails(projectId: string): void {
-    this.subscriptionsService
-      .getAllByProject(projectId)
-      .subscribe(s => (this.combinedData = s));
+    this.subscriptionsService.getAllByProject(projectId).subscribe({
+      next: response => {
+        this.subscriptionsInProject.set(response);
+      },
+      error: error => console.error(error),
+    });
+  }
+
+  showApproveDialog(subscription: iInscricao): void {
+    this.subscriptionSelected.set(subscription);
+    this.visibleApprove.set(true);
+  }
+
+  showRepproveDialog(subscription: iInscricao): void {
+    this.subscriptionSelected.set(subscription);
+    this.visibleRepprove.set(true);
   }
 }
