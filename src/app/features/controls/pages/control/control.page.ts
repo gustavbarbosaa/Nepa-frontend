a6179bfea9765d4292979dc6e226ff3cb66a112c
gustavbarbosaa <gustavoaraujohab@gmail.com>
2025-08-08 11:33:52 -0300

    cria componente-page de control

---
+++ src/app/features/controls/pages/control/control.page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-control',
+  imports: [],
+  templateUrl: './control.page.html',
+  styleUrl: './control.page.css',
+})
+export class ControlPage {}
