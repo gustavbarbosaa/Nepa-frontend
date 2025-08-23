19ca2b8423f88482c1aa86ac78743762ec3bfcc1
gustavbarbosaa <gustavoaraujohab@gmail.com>
2025-08-23 11:29:42 -0300

    adiciona tabs para cada semana no mes

--- src/app/features/projects/components/form-frequency/form-frequency.component.ts
+++ src/app/features/projects/components/form-frequency/form-frequency.component.ts
@@ -5,21 +5,32 @@ import {
   Input,
   OnInit,
   Output,
+  signal,
 } from '@angular/core';
 import {
   FormControl,
   FormGroup,
   NonNullableFormBuilder,
+  ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
 import { InputFormComponent } from '@shared/components/input-form/input-form.component';
 import { TextAreaComponent } from '@shared/components/text-area/text-area.component';
 import { Button } from 'primeng/button';
 import { Dialog } from 'primeng/dialog';
+import { TabPanel, TabsModule } from 'primeng/tabs';
 
 @Component({
   selector: 'app-form-frequency',
-  imports: [Dialog, Button, InputFormComponent, TextAreaComponent],
+  imports: [
+    Dialog,
+    Button,
+    InputFormComponent,
+    TextAreaComponent,
+    ReactiveFormsModule,
+    TabsModule,
+    TabPanel,
+  ],
   templateUrl: './form-frequency.component.html',
   styleUrl: './form-frequency.component.css',
 })
@@ -28,20 +39,39 @@ export class FormFrequencyComponent implements OnInit {
   @Output() visibleChange = new EventEmitter<boolean>();
   private fb = inject(NonNullableFormBuilder);
 
-  form!: FormGroup;
+  forms: { [key: number]: FormGroup } = {};
+  activeTab = 1;
 
   ngOnInit(): void {
-    this.form = this.fb.group({
-      realizada_em: ['', [Validators.required]],
-      tempo_inicio: ['', [Validators.required]],
-      tempo_termino: ['', [Validators.required]],
-      descricao: ['', [Validators.required]],
-      observacao: ['', [Validators.required]],
-    });
+    for (let i = 1; i <= 4; i++) {
+      this.forms[i] = this.fb.group({
+        realizada_em: ['', Validators.required],
+        tempo_inicio: ['', Validators.required],
+        tempo_termino: ['', Validators.required],
+        descricao: ['', Validators.required],
+        observacao: ['', Validators.required],
+        alunos_presentes: this.fb.array([]),
+      });
+    }
   }
 
-  getControl<T = string>(controlName: string): FormControl<T> {
-    return this.form.get(controlName) as FormControl<T>;
+  submit(): void {
+    const formAtual = this.forms[this.activeTab];
+
+    console.log(formAtual);
+
+    if (formAtual.invalid) {
+      formAtual.markAllAsTouched();
+      return;
+    }
+
+    const payload = formAtual.value;
+    console.log(payload);
+    this.closeDialog();
+  }
+
+  getControl<T = string>(tab: number, controlName: string): FormControl<T> {
+    return this.forms[tab].get(controlName) as FormControl<T>;
   }
 
   closeDialog(): void {
