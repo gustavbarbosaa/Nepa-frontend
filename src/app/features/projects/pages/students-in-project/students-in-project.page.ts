83230ff96c8f6dc8085f393b5dfdf07e34629f6a
gustavbarbosaa <gustavoaraujohab@gmail.com>
2025-08-28 09:42:52 -0300

    remove componentes nao utilizados

--- src/app/features/projects/pages/students-in-project/students-in-project.page.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-students-in-project',
-  imports: [],
-  templateUrl: './students-in-project.page.html',
-  styleUrl: './students-in-project.page.css',
-})
-export class StudentsInProjectPage {}
